a66b7a94aeffef01e5154dc67b05fe0893f58934
Max Couch <caxmouch@gmail.com>
2018-11-14 09:24:24 -0500

    adding dropdown measurement menus to cup and spoon

--- SparkProject/kitchen.js
+++ SparkProject/kitchen.js
@@ -1,4 +1,24 @@
+function MeasureCup() {
+    document.getElementById("cup").classList.toggle("show");
+}
 
+function MeasureSpoon() {
+    document.getElementById("spoon").classList.toggle("show");
+}
+
+window.onclick = function(event) {
+    if (!event.target.matches('.measuringTool')) {
+  
+      var dropdowns = document.getElementsByClassName("measurements");
+      var i;
+      for (i = 0; i < dropdowns.length; i++) {
+        var openDropdown = dropdowns[i];
+        if (openDropdown.classList.contains('show')) {
+          openDropdown.classList.remove('show');
+        }
+      }
+    }
+  }
 
 /*
 Need Tools, Ingredients, Appliances, and Functions
--- SparkProject/recipeFetch.js
+++ SparkProject/recipeFetch.js
@@ -3,19 +3,69 @@ let state = {
 }
 // http:www.recipepuppy.com/api/
 
-let recipeURL = "http:www.recipepuppy.com/api/?i=onions,garlic&q=omelet&p=3 ";
+let recipeURL = 'http://www.recipepuppy.com/api/?i=onions,garlic&q=omelet&p=3';
 let query= "";
 
 let recipeFetch = function() {
-    fetch("http:www.recipepuppy.com/api/?i=onions,garlic&q=omelet&p=3 ", {
-        // method: 'GET', 
-        mode: 'no-cors', 
-        /*headers: {
+    fetch(recipeURL, {
+        method: 'GET', 
+        mode: 'cors', 
+        headers: {
             "Accept:": "application/json",
             'Access-Control-Allow-Origin':'*'
-        }*/
+        }
     }).then(req => console.log(req));
     // .then(result => console.log(result))
 }
 
 recipeFetch();
+
+//once I get data from API
+//.then((data) => )
+
+//updateRecipe();
+//create parapgraph element to show fetched recipe
+
+function updateRecipe(){
+    let newPara = document.createElement('p')
+    //newPara.innerText = information recieved from API to put into paragraph on webpage;
+    // add new p element as a child of results-container article
+    //document.getElementById("results-container").appendChild(newPara);
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+//let url = 'http://www.recipepuppy.com/api/?i=onions,garlic&q=omelet&p=3'
+//AJAX
+function getRecipe(url, func){
+    //first: we need to obtain XHR
+    var xhr = new XMLHttpRequest();
+    //second: define onreadystatechange
+    xhr.onreadystatechange = function(){
+        if (this.readyState == 4 && this.status == 200){
+            func(this);
+        }
+    }
+    //third: prepare request
+    xhr.open("GET", url, true);
+    //finally: send
+    xhr.send();
+}
+
+function newRecipe(xhr){
+    //JSON.parse will convert a JSON-formatted string to a javascript object
+    var res = JSON.parse(xhr.responseText)
+    console.log(res);
+    //can use this object however we want
+}
+
