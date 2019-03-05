5844d8cafa8c7cd091fa0924d702edc63c8fadfc
Max Couch <caxmouch@gmail.com>
2019-03-05 10:53:24 -0500

    intro to Maximilian's Angular, and shuffling H2 info in application properties

--- Projekt-UI/src/app/app.module.ts
+++ Projekt-UI/src/app/app.module.ts
@@ -1,5 +1,10 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+/**
+ * added above module to the module.ts to allow angular to recognize the use of ngModel in the app.component.html
+ * must be imported below
+ */
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -10,7 +15,8 @@ import { AppComponent } from './app.component';
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
