6ee262111665217bfa18ea746d78d29a96e543d0
Max Couch <caxmouch@gmail.com>
2019-03-05 15:28:47 -0500

    simple styling of app.component using bootstrap in html

--- Projekt-UI/src/app/app.component.ts
+++ Projekt-UI/src/app/app.component.ts
@@ -5,7 +5,7 @@
 import { Component } from '@angular/core';
 
 @Component({
-  selector: 'app-root', /**the selector is how Angular can take the entire component and insert it into another html file*/
+  selector: 'app-root', /** the selector is how Angular can take the entire component and insert it into another html file*/
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
--- Projekt-UI/src/app/calculator/calculator.component.ts
+++ Projekt-UI/src/app/calculator/calculator.component.ts
@@ -1,9 +1,12 @@
 import { Component, OnInit } from '@angular/core';
 
 @Component({
-  selector: 'app-calculator',
-  templateUrl: './calculator.component.html',
-  styleUrls: ['./calculator.component.css']
+  selector: 'app-calculator', /** it is possible to use [app-calculator] to reference this component as an attribute of a div,
+  as well as putting a . in front to use as a class within a div to achieve the same result as adding the app-calculator tag in
+  the app.component.html */
+  templateUrl: './calculator.component.html', /** it is possible to use `<html-tags></html-tags>` to achieve the same effect, as long as the html is not extensive */
+  styleUrls: ['./calculator.component.css'] /** the same above can be done here by removing "Urls" from the variable name, and simply writing
+  css code within backticks */
 })
 export class CalculatorComponent implements OnInit {
 
